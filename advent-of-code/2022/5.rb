13cadc2ec82eedfeee97df45aae1fc665e4c0a4f
Nikos Kalaitzian <nikoskalai@gmail.com>
2022-12-05 20:36:26 +0200

    fixes in 5th advent of code

--- advent-of-code/2022/5.rb
+++ advent-of-code/2022/5.rb
@@ -1,31 +1,28 @@
-file = "5.input.txt"
-file_contents = File.open(file, "r").read
+file = '5.input.txt'
+file_contents = File.open(file, 'r').read
 
 crates = file_contents.split("\n").first(8).reverse.each.with_object({}) do |line, crates|
-  arr = line.split("")
+  arr = line.split('')
   columns = (0..8).map { |i| arr[1 + i * 4] }
   columns.each.with_index(1) do |c, i|
     crates[i] = [] if crates[i].nil?
-    crates[i] << c if c != " "
+    crates[i] << c if c != ' '
   end
 end
 
-instructions = file_contents.
-  split("\n")[10..-1].
-  map { |l| l.gsub("move ", "").gsub(" from", "").gsub(" to", "") }.
-  map do |instr|
-  items = instr.split(" ")[0].to_i
-  from = instr.split(" ")[1].to_i
-  to = instr.split(" ")[2].to_i
-  [items, from, to]
-end
-
+instructions = file_contents.split("\n")[10..].
+               map { |l| l.gsub('move ', '').gsub(' from', '').gsub(' to', '') }.
+               map do |instr|
+                 items = instr.split(' ')[0].to_i
+                 from = instr.split(' ')[1].to_i
+                 to = instr.split(' ')[2].to_i
+                 [items, from, to]
+               end
 instructions.each do |items, from, to|
-  puts "Instructions: Move #{items} from #{from} to #{to}"
   items.times { crates[to].push(crates[from].pop) }
 end
 
-crates.to_a.each do |k, v|
+crates.to_a.each do |_k, v|
   print "#{v.last}"
 end
 puts
@@ -34,11 +31,11 @@ puts
 # =================== 2 ==================
 
 crates_9001 = file_contents.split("\n").first(8).reverse.each.with_object({}) do |line, crates_9001|
-  arr = line.split("")
+  arr = line.split('')
   columns = (0..8).map { |i| arr[1 + i * 4] }
   columns.each.with_index(1) do |c, i|
     crates_9001[i] = [] if crates_9001[i].nil?
-    crates_9001[i] << c if c != " "
+    crates_9001[i] << c if c != ' '
   end
 end
 
@@ -46,7 +43,7 @@ instructions.each do |items, from, to|
   crates_9001[to] += crates_9001[from].pop(items)
 end
 
-crates_9001.to_a.each do |k, v|
+crates_9001.to_a.each do |_k, v|
   print "#{v.last}"
 end
 puts
